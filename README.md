664034c561ac9fba44d5635a433b53211a7fa78b
Matthew McPartlon <matthewmcpartlon@Matthews-MacBook-Pro.local>
2023-01-28 14:05:06 -0600

    adding example for post-processing

--- README.md
+++ README.md
@@ -6,6 +6,59 @@ TODO: Examples and Inference
 
 This repo contains code for AttnPacker
 
+# Examples
+
+## Run Post-Process Procedure on a PDB File
+```
+usage: project_rotamer_example.py [-h] [--pdb_path_out PDB_PATH_OUT] [--steric_wt STERIC_WT] [--steric_tol_allowance STERIC_TOL_ALLOWANCE]
+                                  [--steric_tol_frac STERIC_TOL_FRAC] [--steric_hbond_allowance STERIC_HBOND_ALLOWANCE]
+                                  [--max_optim_iters MAX_OPTIM_ITERS] [--torsion_loss_wt TORSION_LOSS_WT] [--device DEVICE]
+                                  pdb_path_in
+
+Project Protein Sidechains onto Continuous Rotamer and Minimize Steric Clashes
+
+positional arguments:
+  pdb_path_in           path to input pdb
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --pdb_path_out PDB_PATH_OUT
+                        path to save projected pdb to (dafaults to post-processed-<input pdb name>.pdb (default: None)
+  --steric_wt STERIC_WT
+                        weight to use for steric clash loss (default: 0.2)
+  --steric_tol_allowance STERIC_TOL_ALLOWANCE
+                        subtract this number from all atom vdW radii (default: 0.05)
+  --steric_tol_frac STERIC_TOL_FRAC
+                        set vdW radii to steric_tol_frac*vdW(atom_type) (default: 0.9)
+  --steric_hbond_allowance STERIC_HBOND_ALLOWANCE
+                        subtract this number from the sum of vdW radii for hydrogen bond donor/acceptor pairs (default: 0.6)
+  --max_optim_iters MAX_OPTIM_ITERS
+                        maximum number of iterations to run optimization procedure for (default: 250)
+  --torsion_loss_wt TORSION_LOSS_WT
+                        penalize average deviaiton from initial dihedral angles with this weight (default: 0)
+  --device DEVICE       device to use when running this procedure (default: cpu)
+
+
+(py38)[mmcpartlon@raptorx11 AttnPacker]$ python protein_learning/examples/project_rotamer_example.py ./protein_learning/examples/pdbs/T1057-predicted.pdb --steric_tol_allowance 0 --steric_tol_frac 0.95 --max_optim_iters 200 --device cuda:0
+
+[fn: project_onto_rotamers] : Using device cuda:0
+[INFO] Beginning rotamer projection
+[INFO] Initial loss values
+   [RMSD loss] = 0.103
+   [Steric loss] = 0.035
+   [Angle Dev. loss] = 0.0
+
+beginning iter: 0, steric weight: 0.2
+[INFO] Final Loss Values
+   [RMSD loss] = 0.064
+   [Steric loss] = 0.002
+   [Angle Dev. loss] = 0.001
+
+Saving to: ./protein_learning/examples/pdbs/post-processed-T1057-predicted.pdb
+Finished in 3.32 seconds
+```
+
+
 ## common
 
 The common folder contains protein specific constants such as residue names, atom types, side-chain dihedral
---
+++ protein_learning/examples/project_rotamer_example.py
@@ -0,0 +1,83 @@
+import os
+import sys
+from protein_learning.protein_utils.sidechains.project_sidechains import (
+    project_onto_rotamers,
+)
+from protein_learning.common.data.data_types.protein import Protein
+import protein_learning.common.protein_constants as pc
+from protein_learning.common.data.datasets.utils import set_canonical_coords_n_masks
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+import time
+
+
+
+def project_pdb(
+    pdb_path_in:str,
+    pdb_path_out:str=None,
+    steric_wt:float=0.1,
+    steric_tol_allowance=0.05,
+    steric_tol_frac = 0.9,
+    steric_hbond_allowance: float = 0.6,
+    max_optim_iters: int = 200,
+    torsion_loss_wt: float = 0.25,
+    device:str = "cpu"
+    ):
+    protein = Protein.FromPDBAndSeq(
+            pdb_path = pdb_path_in,
+            seq=None,
+            atom_tys = pc.ALL_ATOMS,
+            missing_seq = True,
+        )
+    protein = set_canonical_coords_n_masks(protein)
+
+    
+
+    projected_coords, _ = project_onto_rotamers(
+        atom_coords = protein.atom_coords.unsqueeze(0).clone(),
+        atom_mask = protein.atom_masks.unsqueeze(0),
+        sequence = protein.seq_encoding.unsqueeze(0),
+        optim_repeats = 1,
+        steric_clash_weight = steric_wt,
+        steric_loss_kwargs = dict(
+            global_tol_frac=steric_tol_frac,
+            hbond_allowance=steric_hbond_allowance,
+            global_allowance=steric_tol_allowance, 
+            reduction = "sum",
+            p=2
+        ),
+        optim_kwargs = dict(max_iter=max_optim_iters, lr=1e-3,line_search_fn="strong_wolfe"),
+        use_input_backbone = True,
+        torsion_deviation_loss_wt = torsion_loss_wt, #penalize torsion angle deviations
+        device=device,
+    )
+    
+    if pdb_path_out is None:
+        pdb_path_out = os.path.join(
+            os.path.dirname(pdb_path_in),f"post-processed-{os.path.basename(pdb_path_in)}"
+        )
+    print(f"Saving to: {pdb_path_out}")
+    protein.to_pdb(
+        path=pdb_path_out,
+        coords=projected_coords.squeeze(),
+    )
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Project Protein Sidechains onto Continuous Rotamer and Minimize Steric Clashes",  # noqa
+                        epilog='',
+                        formatter_class=ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("pdb_path_in",help="path to input pdb")
+    parser.add_argument("--pdb_path_out",help="path to save projected pdb to (dafaults to post-processed-<input pdb name>.pdb")
+    parser.add_argument("--steric_wt",help="weight to use for steric clash loss",default=0.2,type=float)
+    parser.add_argument("--steric_tol_allowance",help="subtract this number from all atom vdW radii",default=0.05,type=float)
+    parser.add_argument("--steric_tol_frac",help="set vdW radii to steric_tol_frac*vdW(atom_type)",default=0.925,type=float)
+    parser.add_argument("--steric_hbond_allowance",help="subtract this number from the sum of vdW radii for hydrogen bond donor/acceptor pairs",default=0.6,type=float)
+    parser.add_argument("--max_optim_iters",help="maximum number of iterations to run optimization procedure for",type=int,default=250)
+    parser.add_argument("--torsion_loss_wt",help="penalize average deviaiton from initial dihedral angles with this weight",type=float, default=0)
+    parser.add_argument("--device",help="device to use when running this procedure",type=str,default="cpu")
+    args = parser.parse_args(sys.argv[1:])
+    start = time.time()
+    project_pdb(**vars(args))
+    print(f"Finished in {round(time.time()-start,3)} seconds")
+
